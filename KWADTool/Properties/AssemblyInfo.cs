d509868cea69771ab2b72da3d0e6e437d489bfdd
Cyberboy2000 <37832729+Cyberboy2000@users.noreply.github.com>
2025-07-16 10:39:53 +0200

    Bump version number

--- KWADTool/Properties/AssemblyInfo.cs
+++ KWADTool/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using CommandLine;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.1")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
 
 // from CommandLineParser.Text
 [assembly: AssemblyLicense(
